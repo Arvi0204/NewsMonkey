b338f9d142fbb90d4466daf7330cc9383c9a418a
Arvind M Bharadwaj <arvi2014.m@gmail.com>
2024-10-10 16:13:39 +0530

    Added fetch api to populate cards using NewsAPI

--- src/NewsItem.js
+++ src/NewsItem.js
@@ -7,10 +7,10 @@ export class NewsItem extends Component {
         return (
             <div className='my-3'>
                 <div className="card" style={{ width: '18rem' }}>
-                    <img src={imgUrl} className="card-img-top" alt="" />
+                    <img src={imgUrl?imgUrl:"https://ichef.bbci.co.uk/news/1024/branded_news/e4ce/live/1205b210-8591-11ef-addc-5556603eb4c1.jpg"} className="card-img-top" alt="" />
                     <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{desc}...</p>
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{desc}</p>
                         <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-primary btn-sm">Read More</a>
                     </div>
                 </div>
