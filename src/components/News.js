462af1cc09a7392d23b579147508581722ef1264
Arvind M Bharadwaj <arvi2014.m@gmail.com>
2024-10-10 16:51:00 +0530

    Added Next & Previous buttons to display more articles using API

--- src/components/News.js
+++ src/components/News.js
@@ -6,28 +6,66 @@ export class News extends Component {
     super(); // Super constructor must be called to avoid errors
     this.state = {
       articles: this.articles,
-      loading: false
+      loading: false,
+      page:1
     }
   }
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=c22fbad0692842d1971194477d5bf4a0"
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c22fbad0692842d1971194477d5bf4a0&page=1&pageSize=20"
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    // console.log(parsedData);
+    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
+  }
+
+  handleNext = async () => {
+    // console.log("Next");
+    if(this.state.page+1 > Math.ceil(this.state.totalResults/20)){
+
+    }
+    else{
+
+      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c22fbad0692842d1971194477d5bf4a0&page=${this.state.page+1}&pageSize=20`
+      let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+
+    this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles
+    })
+  }
+  }
+  handlePrev = async () => {
+    // console.log("Prev");
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c22fbad0692842d1971194477d5bf4a0&page=${this.state.page-1}&pageSize=20`
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({articles: parsedData.articles})
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles
+    })
   }
+  
+  
   render() {
     return (
       <div className="container my-3">
         <h2>Welcome to NewsMonkey, today's headlines</h2>
         <div className="row">
-          {this.state.articles.map((element) => {
+          {this.state.articles && this.state.articles.map((element) => {
             return (
-              <div className="col-md-4" key = {element.url} >
+              <div className="col-md-4" key={element.url} >
                 <NewsItem title={element.title} desc={element.description} imgUrl={element.urlToImage} newsUrl={element.url} />
               </div>
             )
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page<=1} type="button" className="btn btn-danger" onClick={this.handlePrev}>&larr; Previous</button>
+          <button type="button" className="btn btn-success" onClick={this.handleNext}>Next &rarr;</button>
         </div>
       </div>
     )
