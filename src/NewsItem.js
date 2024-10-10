2fcee939b9e0d75c81c51929414b37c24a5a512f
Arvind M Bharadwaj <arvi2014.m@gmail.com>
2024-10-10 15:13:32 +0530

    Added constructor and state part in News.js.

--- src/NewsItem.js
+++ src/NewsItem.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+    
     render() {
-        let { title, description } = this.props;
+        let { title, description, newsUrl, imgUrl} = this.props;
         return (
-            <div>
+            <div className='my-3'>
                 <div className="card" style={{ width: '18rem' }}>
-                    <img src="https://static.files.bbci.co.uk/ws/simorgh-assets/public/sport/images/metadata/poster-1024x576.png" className="card-img-top" alt="..." />
+                    <img src={imgUrl} className="card-img-top" alt="image URL" />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="/newsDetail" className="btn btn-primary">Go somewhere</a>
+                        <a href="/newsDetail/" className="btn btn-primary btn-sm">Read More</a>
                     </div>
                 </div>
             </div>
