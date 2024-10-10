40691c051e29966880f64a5a87374d123d79ce44
Arvind M Bharadwaj <arvi2014.m@gmail.com>
2024-10-10 15:42:51 +0530

    Spelling mistake in newsUrl paramter in News.js, fixed

--- src/components/News.js
+++ src/components/News.js
@@ -59,7 +59,7 @@ export class News extends Component {
           {this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key = {element.url} >
-                <NewsItem title={element.title} description={element.description} imgUrl={element.urlToImage} newsURL={element.url} />
+                <NewsItem title={element.title} description={element.description} imgUrl={element.urlToImage} newsUrl={element.url} />
               </div>
             )
           })}
