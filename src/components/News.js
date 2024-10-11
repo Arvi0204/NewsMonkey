5f099dc4075122016d33d8a5d65e9aeee1c1420d
Arvind M Bharadwaj <arvi2014.m@gmail.com>
2024-10-11 17:30:57 +0530

    added top laoding bar. Working progess. API limit reached

--- src/components/News.js
+++ src/components/News.js
@@ -30,11 +30,13 @@ export class News extends Component {
     document.title = `NewsMonkey - ${this.capitalizeFirst(props.category)}`;
   }
   async updateNews() {
+    this.props.setProgress(0);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c22fbad0692842d1971194477d5bf4a0&page=${this.state.page}&pageSize=${this.props.pageSize}`
     this.setState({ loading: true })
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({ articles: parsedData.articles, loading: false, totalResults: parsedData.totalResults })
+    this.props.setProgress(100)
   }
   async componentDidMount() {
     this.updateNews();
