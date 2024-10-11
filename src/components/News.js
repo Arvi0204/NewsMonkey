1d06d4991c109d24e72b9c261ac09d9840ec33a4
Arvind M Bharadwaj <arvi2014.m@gmail.com>
2024-10-11 13:06:37 +0530

    Added author, time stamp and badge

--- src/components/News.js
+++ src/components/News.js
@@ -66,7 +66,7 @@ export class News extends Component {
           {!this.state.loading && this.state.articles && this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.url} >
-                <NewsItem title={element.title} desc={element.description} imgUrl={element.urlToImage} newsUrl={element.url} />
+                <NewsItem title={element.title} desc={element.description} imgUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
               </div>
             )
           })}
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,14 +3,19 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
     render() {
-        let { title, desc, imgUrl, newsUrl } = this.props;
+        let { title, desc, imgUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className='my-3'>
                 <div className="card">
-                    <img src={imgUrl?imgUrl:"https://ichef.bbci.co.uk/news/1024/branded_news/e4ce/live/1205b210-8591-11ef-addc-5556603eb4c1.jpg"} className="card-img-top" alt="" />
+                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:1,left:"95%"}}>
+                        {source}
+                        <span className="visually-hidden">unread messages</span>
+                    </span>
+                    <img src={imgUrl ? imgUrl : "https://ichef.bbci.co.uk/news/1024/branded_news/e4ce/live/1205b210-8591-11ef-addc-5556603eb4c1.jpg"} className="card-img-top" alt="" />
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
+                        <h5 className="card-title">{title} </h5>
                         <p className="card-text">{desc}</p>
+                        <p className="card-text"><small className="text-body-secondary">By {author ? author : "Unknown"} on {new Date(date).toUTCString()}</small></p>
                         <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-dark btn-sm">Read More</a>
                     </div>
                 </div>
